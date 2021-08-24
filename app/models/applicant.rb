a8c400d4cab9d8609e0188719fb73aaa129f22af
artex <htettunlu15898@gmail.com>
2021-08-24 15:53:41 +0630

    Second_Test

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -2,9 +2,9 @@ class Applicant < ApplicationRecord
     validates :name, presence: true,  length: { maximum: 100 }
     validates :profile_photo, presence: true
     validates :dob, presence: true
-    validates :phone_no1, presence: true, length: { maximum: 30 }
-    validates :phone_no2, length: { maximum: 30 }
-    validates :email, presence: true, uniqueness: true, length: { maximum: 100 }
+    validates :phone_no1, presence: true, length: { maximum: 30 },:numericality => true, :length => { :minimum => 6, :maximum => 13 }
+    validates :phone_no2, length: { maximum: 30 },:numericality => true, :length => { :minimum => 6, :maximum => 13 }
+    validates :email, presence: true, uniqueness: true, length: { maximum: 100 }, format: { with: URI::MailTo::EMAIL_REGEXP }
     validates :current_address, presence: true, length: { maximum: 255 }
     validates :hometown_address, length: { maximum: 255 }
     validates :bachelor_university, length: { maximum: 100 }
