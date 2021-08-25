8917a11e0e7df1cdda3cbc9ac989ecb2aef013e3
artex <htettunlu15898@gmail.com>
2021-08-25 09:06:01 +0630

    migration_fix

--- db/migrate/20210820021950_add_programming_to_applicants.rb
+++ db/migrate/20210820021950_add_programming_to_applicants.rb
@@ -1,6 +1,5 @@
 class AddProgrammingToApplicants < ActiveRecord::Migration[6.1]
   def change
-    change_column :applicants, :phone_no1, :integer, limit: N
-    change_column :applicants, :phone_no2, :integer, limit: N
+    add_column :applicants, :programming, :text
   end
 end
